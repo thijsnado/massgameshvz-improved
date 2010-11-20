6f38e731d93d201739725a3397c1d74390356ca7
Thijs de Vries <moger777@gmail.com>
2010-11-20 10:09:57 -0500

    base rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Massgameshvz::Application.config.secret_token = 'cd62a7ab3652dee543dad90ee2e2614f34ef193b0b64e3a0bcd30fdb0a27ef235f50e96c4a372a0a7572ec46370e9e784da20d2fed08c29d0a85e18934e94e56'
