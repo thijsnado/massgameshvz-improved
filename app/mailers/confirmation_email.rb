cbda17ba4be8eb87cbf209f74aaf17f5559a1e64
Thijs de Vries <tdevries.development@gmail.com>
2011-03-20 18:55:50 -0400

    adding from

--- app/mailers/confirmation_email.rb
+++ app/mailers/confirmation_email.rb
@@ -1,11 +1,12 @@
 class ConfirmationEmail < ActionMailer::Base
-  default :from => "noreply@hvz.massgames.org"
+  default :from => "noreply@massgameshvz.heroku.com"
   
   def confirmation_message(user)
     recipients user.email_address
     subject "hvz confirmation"
     sent_on Time.now
     content_type "text/html"
+    from "noreply@massgameshvz.heroku.com"
     @user = user
     @confirmation_hash = user.confirmation_hash
   end
