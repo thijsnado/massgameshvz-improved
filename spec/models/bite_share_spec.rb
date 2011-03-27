55e4c548dae1f28cb97d56e2f90e92769eed28af
Thijs de Vries <tdevries.development@gmail.com>
2011-03-27 14:54:27 -0400

    getting specs passing again

--- spec/models/bite_share_spec.rb
+++ spec/models/bite_share_spec.rb
@@ -29,7 +29,7 @@ describe BiteShare do
       Timecop.freeze(current_game.start_at)
     end
     it "should extend the life of another zombie participation to the recorded starve time increase" do
-      zombie_participation.bite_shares.first.share(zombie_participation_2)
+      zombie_participation.bite_shares.first(:readonly => false).share(zombie_participation_2)
       zombie_participation_2.zombie_expires_at.should == zombie_expiration_calculation
     end
   end
