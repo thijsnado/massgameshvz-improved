40be0530395bd9188ee1944fab2a99fb7e2c30cf
Thijs de Vries <tdevries.development@gmail.com>
2011-03-27 11:30:36 -0400

    wrapping up bite shares

--- app/models/bite_share.rb
+++ app/models/bite_share.rb
@@ -1,6 +1,7 @@
 class BiteShare < ActiveRecord::Base
   belongs_to :bite_event
   belongs_to :game_participation
+  belongs_to :shared_with, :class_name => 'GameParticipation'
   
   def self.not_used
     where(:used => false)
@@ -10,6 +11,12 @@ class BiteShare < ActiveRecord::Base
     game_participation.zombie_expires_at = self.bite_event.zombie_expiration_calculation 
     game_participation.save
     self.used = true
+    self.shared_with = game_participation
     self.save
+    bite_share_event = BiteShareEvent.new
+    bite_share_event.game_participation = self.game_participation
+    bite_share_event.target_object = self
+    bite_share_event.occured_at = Time.now
+    bite_share_event.save
   end
 end
