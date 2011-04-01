2a9deba1c22affa7f62984635dd530678ba52816
Thijs de Vries <tdevries.development@gmail.com>
2011-04-01 19:03:22 -0400

    adding ability to add email domain

--- app/controllers/admin/email_domains_controller.rb
+++ app/controllers/admin/email_domains_controller.rb
@@ -7,6 +7,19 @@ class Admin::EmailDomainsController < AdminController
     @email_domain = EmailDomain.new
   end
   
+  def edit
+    @email_domain = EmailDomain.find(params[:id])
+  end
+  
+  def update
+    @email_domain = EmailDomain.find(params[:id])
+    if @email_domain.update_attributes(params[:email_domain])
+      redirect_to admin_email_domains_url
+    else
+      render :action => 'new'
+    end
+  end
+  
   def create
     @email_domain = EmailDomain.new(params[:email_domain])
     if @email_domain.save
